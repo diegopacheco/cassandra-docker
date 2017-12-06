e883c0344e37f3a63ff470001951ae9924083b06
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-12-05 18:15:10 -0800

    change running options

--- start-cass.sh
+++ start-cass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /cassandra/apache-cassandra-3.9/
-/cassandra/apache-cassandra-3.9/bin/cassandra > /cassandra/cassandra.txt &
+/cassandra/apache-cassandra-3.9/bin/cassandra -R > /cassandra/cassandra.txt &
