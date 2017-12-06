4c5991842a78396455de24a1b976deb58be43b00
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-12-05 18:24:32 -0800

    fix cluster config

--- start-cass.sh
+++ start-cass.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sed -i s/@CASS_NODE_IP/$(hostname -i)/g /cassandra/apache-cassandra-3.9/conf/cassandra.yaml
+
 cd /cassandra/apache-cassandra-3.9/
 /cassandra/apache-cassandra-3.9/bin/cassandra -R > /cassandra/cassandra.txt &
